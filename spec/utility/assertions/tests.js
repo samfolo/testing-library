89a6167003741bc370e625f51b4b91f487c0d7be
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-06 19:45:47 +0000

    fix error

--- spec/utility/assertions/tests.js
+++ spec/utility/assertions/tests.js
@@ -1,5 +1,5 @@
 // 'toBe':
 expect(5).toBe(5); // should be true
-expect(4).toBe(4); //should be false
+expect(4).toBe('4'); //should be false
 expect('two').toBe(2); // to be false
-expect('six').toBe(6); // should be true
+expect('six').toBe('six'); // should be true
