d6007e3b1543e414b3f5072d3810679a0c0a72be
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-07 00:09:23 +0000

    implement 'not' versions of all assertions

--- spec/utility/assertions/Expect.js
+++ spec/utility/assertions/Expect.js
@@ -1,6 +1,7 @@
 class Expect {
   constructor(expected = null) {
     this.expected = expected;
+    this.not = new Not(expected)
   }
 
   toBe = actual => {
@@ -63,6 +64,75 @@ class Expect {
       console.log(`    expected function to throw an error :: Test Failed`); return true;
     } catch (e) {
       console.log(`    expected function to throw an error :: Test Passed`); return false;
+    }
+  }
+}
+
+class Not {
+  constructor(expected = null) {
+    this.expected = expected;
+  }
+
+  toBe = actual => {
+    validateAssertion(this.expected, actual);
+    if (this.expected !== actual) {
+      console.log(`    expected ${this.expected} not to be ${actual} :: Test Passed`); return true;
+    } else {
+      console.log(`    expected ${this.expected} not to be ${actual} :: Test Failed`); return false;
+    };
+  }
+
+  toEqual = actual => {
+    validateAssertion(this.expected, actual);
+    if (this.expected != actual) {
+      console.log(`    expected ${this.expected} not to equal ${actual} :: Test Passed`); return true;
+    } else {
+      console.log(`    expected ${this.expected} not to equal ${actual} :: Test Failed`); return false;
+    };
+  }
+
+  toContain = actual => {
+    validateAssertion(this.expected, actual);
+    let result = true;
+    if (typeof actual === 'object') {
+      try {
+        actual.forEach(el => {
+          result = result && this.expected.includes(el);
+        });
+        if (!result) { 
+          console.log(`    expected ${this.expected} not to contain ${actual} :: Test Passed`); return true;
+        }
+      } catch (e) {
+        throw new Error('Invalid assertion');
+      }
+    } else if (!this.expected.includes(actual)) { 
+      console.log(`    expected ${mapArrayToString(this.expected)} not to contain ${actual} :: Test Passed`); return true;
+    };
+    console.log(`    expected ${mapArrayToString(this.expected)} not to contain ${actual} :: Test Failed`); return false;
+  }
+
+  toAllBe(actual) {
+    validateAssertion(this.expected, actual);
+    let result = true;
+    try {
+      this.expected.forEach(el => {
+        result = result && el === actual;
+      });
+      if (!result) { 
+        console.log(`    expected ${mapArrayToString(this.expected)} not to all be ${actual} :: Test Passed`); return true;
+      }
+    } catch (e) {
+      throw new Error('Invalid assertion');
+    }
+    console.log(`    expected ${mapArrayToString(this.expected)} not to all be ${actual} :: Test Failed`); return false;
+  }
+
+  toThrowError() {
+    try {
+      this.expected();
+      console.log(`    expected function not to throw an error :: Test Passed`); return true;
+    } catch (e) {
+      console.log(`    expected function not to throw an error :: Test Failed`); return false;
     }
   }
 }
--- spec/utility/describe/tests.js
+++ spec/utility/describe/tests.js
@@ -1,9 +1,9 @@
 describe('toBe', () => {
   it('deeply compares (===) an actual and expected value', () => {
-    expect(5).toBe(5); // should be true
-    expect(4).toBe('4'); //should be false
-    expect('two').toBe(2); // to be false
-    expect('six').toBe('six'); // should be true
+    expect(5).toBe(5);
+    expect(4).not.toBe('4');
+    expect('two').not.toBe(2);
+    expect('six').toBe('six');
   });
 
   it('is validates that 1 + 1 is 2', () => {
@@ -11,29 +11,29 @@ describe('toBe', () => {
   });
 });
 
-try { expect().toBe(true) } catch (e) { console.log('error as expected, F'); } // should throw error NO_ACTUAL
-try { expect(true).toBe() } catch (e) { console.log('error as expected, F'); } // should throw error NO_EXPECTED
-try { expect().toBe() } catch (e) { console.log('error, as expected, F'); } // should throw error NO_ASSERTION
+try { expect().toBe(true) } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
+try { expect(true).toBe() } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
+try { expect().toBe() } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
 
 describe('toEqual', () => {
   it('lightly compares (==) an actual and expected value', () => {
-    expect(5).toEqual('5'); // should be true
-    expect([]).toEqual([]); // should be false
-    expect(1 + 1).toEqual(2); //should be true
-    expect(1 + 1).toEqual(3); // should be false
+    expect(5).toEqual('5');
+    expect([]).not.toEqual([]);
+    expect(1 + 1).toEqual(2);
+    expect(1 + 1).toEqual(3);
   });
 });
 
-try { expect().toEqual('x') } catch (e) { console.log('error as expected, F'); } // should throw error NO_ACTUAL
-try { expect({ a: 'b'}).toEqual() } catch (e) { console.log('error as expected, F'); } // should throw error NO_EXPECTED
-try { expect().toEqual() } catch (e) { console.log('error, as expected, F'); } // should throw error NO_ASSERTION
+try { expect().toEqual('x') } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
+try { expect({ a: 'b'}).toEqual() } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
+try { expect().toEqual() } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
 
 describe('toContain', () => {
   it('checks if an element or elements (all) exist(s) within the expected array', () => {
-    expect([3, 2, 1]).toContain([1, 2, 3]); // should be true
-    expect(['4', '3', '2', '1']).toContain('4'); // should be true
-    expect(['4', '3', '2', '1']).toContain(4); // should be false
-    expect([(() => { return 4 * 4})(), '6']).toContain(16); // should be true
+    expect([3, 2, 1]).toContain([1, 2, 3]);
+    expect(['4', '3', '2', '1']).toContain('4');
+    expect(['4', '3', '2', '1']).not.toContain(4);
+    expect([(() => { return 4 * 4})(), '6']).toContain(16);
   });
 
   it("can validate the contents of ['this', 'that', 'the other']", () => {
@@ -43,27 +43,28 @@ describe('toContain', () => {
   });
 });
 
-try { expect(1, 2, 3, 4).toContain(3) } catch (e) { console.log('error, as expected, F'); } // should throw error INVALID_ASSERTION
-try { expect().toContain(4 % 2) } catch (e) { console.log('error as expected, F'); } // should throw error NO_ACTUAL
-try { expect(JSON.stringify('this')).toContain() } catch (e) { console.log('error as expected, F'); } // should throw error NO_EXPECTED
-try { expect().toContain() } catch (e) { console.log('error, as expected, F'); } // should throw error NO_ASSERTION
+try { expect(1, 2, 3, 4).toContain(3) } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
+try { expect().toContain(4 % 2) } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
+try { expect(JSON.stringify('this')).toContain() } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
+try { expect().toContain() } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
 
 describe('toAllBe', () => {
   it('checks if all elements within the expected array evaluate to a particular value', () => {
-    expect([true, true, true]).toAllBe(true); // should be true
-    expect([true, false, true]).toAllBe(true); // should be false
-    expect([1 + 1, 2 + 2, 5]).toAllBe(5); // should be false
-    expect([typeof 'woof', typeof 'meow']).toAllBe('string'); // should be true
+    expect([true, true, true]).toAllBe(true);
+    expect([true, false, true]).not.toAllBe(true);
+    expect([1 + 1, 2 + 2, 5]).not.toAllBe(5);
+    expect([typeof 'woof', typeof 'meow']).toAllBe('string');
   });
 });
 
 describe('toThrowError', () => {
   it('anticipates an error held in a function', () => {
     expect(() => { throw new Error }).toThrowError();
-    expect(() => { 1 - 'teeth' }).toThrowError();
+    expect(() => { throw 'something' }).toThrowError();
+    expect(() => { return ':)' }).not.toThrowError();
   });
 });
 
-try { expect().toAllBe('number') } catch (e) { console.log('error as expected, F'); } // should throw error NO_ACTUAL
-try { expect(...Array(10)).toAllBe() } catch (e) { console.log('error as expected, F'); } // should throw error NO_EXPECTED
-try { expect().toAllBe() } catch (e) { console.log('error, as expected, F'); } // should throw error NO_ASSERTION
+try { expect().toAllBe('number') } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
+try { expect(...Array(10)).toAllBe() } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
+try { expect().toAllBe() } catch (e) { console.log('[Caught Error], Invalid assertion handled'); }
